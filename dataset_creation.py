485a7bbe3a4ff57ac4523e171d7f5f26ee05d330
bmanikan <balamanikandanj2@gmail.com>
2021-04-09 18:07:09 +0530

    modification

--- dataset_creation.py
+++ dataset_creation.py
@@ -11,42 +11,25 @@ import random
 
 
 class clipDataset(Dataset):
-  def __init__(self,df,dataPath,mode='train',clip_len=50):
-    '''
-    partition the dataset into train and test
-    '''
+  def __init__(self,df,dataPath,ids,clip_len=50):
+    assert type(ids) == list, "expectint a list of strings"
     self.df = df
-    self.mode = mode
     self.clip_len = clip_len
-    ids = df[0].unique()
-    train_size = int(0.8*len(ids))
-    test_size = len(ids) - train_size
-    train_ids,test_ids = random_split(ids,[train_size,test_size],generator=torch.Generator().manual_seed(43))
-    self.train_files,self.test_files = {i:[] for i in train_ids},{i:[] for i in test_ids}
+    self.data_files = {i:[] for i in ids}
     for root,dir,files in os.walk(dataPath):
       for f in files:
         id = f.split('_')[0]
-        if id in train_ids:
-          self.train_files[id].append(os.path.join(root,f))
-        elif id in test_ids:
-          self.test_files[id].append(os.path.join(root,f))
+        if id in ids:
+          self.data_files[id].append(os.path.join(root,f))
     random.seed(43)
   
   def __len__(self):
-    '''
-    returns length
-    '''
     return len(self.df)
   
   def __getitem__(self, index):
-    if self.mode == 'train':
-      files = self.train_files
-    elif self.mode == 'test':
-      files = self.test_files
-    else:
-      raise Exception("invalid mode")
+    files = self.data_files
     id = index % len(files)
-    path = files[list(files.keys())[id]]
+    path = sorted(files[list(files.keys())[id]])
     rand_n = random.randint(0,len(path)-self.clip_len)
     frame_path = path[rand_n : rand_n+self.clip_len]
     labels = []
@@ -58,4 +41,4 @@ class clipDataset(Dataset):
       tensor_img = transforms.ToTensor()(cv2.cvtColor(cv2.imread(path),cv2.COLOR_BGR2RGB))
       frames.append(tensor_img)
     frames = torch.stack(frames,dim=0)
-    return frames,  torch.IntTensor(labels)
+    return frames, torch.IntTensor(labels)
