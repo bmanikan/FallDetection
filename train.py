5e8d1a6c4a464f43c8d913c2af64920d7c2b50d3
bmanikan <balamanikandanj2@gmail.com>
2021-04-30 19:14:42 +0530

    modified for 60 frame dataset

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import numpy as np
 from torch import cuda
 import torch
 from utils import transform_image
+import time
 
 def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, save_path):
     """returns trained model"""
@@ -12,14 +13,14 @@ def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, save_path):
         # initialize variables to monitor training and validation loss
         train_loss = 0.0
         valid_loss = 0.0
-        
+        train_acc = 0.0
+        valid_acc = 0.0
         ###################
         # training loop   #
         ###################
         model.train()
         for batch_idx, (data, target) in enumerate(loaders['train']):
-
-            data,target = transform_image(data,target)
+            #data,target = image_embedding(data,target)
             # move to GPU
             if use_cuda:
                 data, target = data.cuda(), target.cuda()
@@ -35,17 +36,21 @@ def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, save_path):
             
             optimizer.step()          
             
-            
-            ## find the loss and update the model parameters accordingly
             ## record the average training loss, using something like
             train_loss = train_loss + ((1 / (batch_idx + 1)) * (loss.data - train_loss))
+            _, pred = torch.max(output, 1)
+            _,tgts = torch.max(target,1) 
+            train_acc += torch.mean((pred==tgts).float())
+            print('.',end='\r')
+
+        train_acc = train_acc / len(loaders['train'])  
+
             
         ######################    
         # validation loop    #
         ######################
         model.eval()
-        for batch_idx, (data, target) in enumerate(loaders['test']):
-            data,target = transform_image(data,target)
+        for batch_idx, (data, target) in enumerate(loaders['valid']):
             # move to GPU
             if use_cuda:
                 data, target = data.cuda(), target.cuda()
@@ -55,14 +60,15 @@ def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, save_path):
             val_loss = criterion(output,target)
             
             valid_loss = valid_loss + ((1 / (batch_idx + 1)) * (val_loss.data - valid_loss))
-            
+            _, pred = torch.max(output, 1)
+            _, tgts = torch.max(target, 1) 
+            valid_acc += torch.mean((pred==tgts).float())
+
+        valid_acc = valid_acc / len(loaders['valid'])
             
         # print training/validation statistics 
-        print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(
-            epoch, 
-            train_loss,
-            valid_loss
-            ))
+        print(f'Epoch: {epoch}\t Training Loss: {train_loss:0.6f}\t Train Accuracy: {train_acc:0.3f} \
+              \nValidation Loss: {valid_loss:.6f} \tValidation Accuracy: {valid_acc:0.3f}')
         
         ## save the model if validation loss has decreased
         if valid_loss <= valid_loss_min:
