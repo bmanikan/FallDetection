dd48e2c7e8d33f6ad02dcd7ecd4373ce6f85773e
bmanikan <balamanikandanj2@gmail.com>
2021-05-29 21:40:48 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,27 +1,35 @@
-import torch
-from torchvision import transforms
-from tqdm.notebook import tqdm
+from collections import defaultdict
 from skimage.metrics import structural_similarity
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import time,os
 from PIL import Image
-from torchvision import transforms
+import tensorflow as tf
+from tensorflow.keras.layers.experimental import preprocessing
+import datetime
+import time
 
+
+t_set = lambda: datetime.datetime.now().astimezone()
+t_diff = lambda t: str(t_set() - t)
+t_stamp = lambda t=None: str(t) if t else str(t_set())
 
 def safedirs(path):
   if not os.path.exists(path):
     os.makedirs(path)
 
-
-def person_detector(image):
+def person_detector(imageMatrix,label,mode,folder):
   '''
   Moving object detection by frame differencing
   input: torch tensor object of shape [batch,50,3,480,640] and labels array
   output: tuple of torch tensor [batch,1,224,224] and label object 
   '''
-  temp_img = image.numpy().transpose(0,2,3,1)
+  info = ['adl','fall']
+  bbox_path = os.path.join(os.getcwd(), 'boundingBox', mode, info[label], folder)
+  logger_t.info(f'Working on creating ... {bbox_path} set')
+  safedirs(bbox_path)
+  temp_img = imageMatrix
   center_points = []
   for i in range(0,temp_img.shape[0] - 2):
     img1 = cv2.medianBlur(cv2.cvtColor(temp_img[i],cv2.COLOR_BGR2GRAY),5)
@@ -34,35 +42,33 @@ def person_detector(image):
     contours = contours[0] if len(contours) == 2 else contours[1] 
     val = np.argmax([cv2.contourArea(c) for c in contours])
     x,y,w,h = cv2.boundingRect(contours[val])
+    cv2.rectangle(img1, (x, y), (x + w, y + h), (36,255,12), 2)
+    path = os.path.join(bbox_path, f'image_{i}.jpg')
+    cv2.imwrite(path, img1)
     center = (int((x+w)/2), int((y+h)/2))
     center_points.append(center)
   return center_points
 
-
 def get_transform():
-  transform = transforms.Compose([transforms.Resize((224,224)),
-                        transforms.ToTensor(),
-                        transforms.Normalize((0.5, ), (0.5, ))])
+  transform = tf.keras.Sequential([
+                                   preprocessing.Resizing(224,224),
+                                   preprocessing.Normalization()
+  ])
   return transform
-    
 
-def image_embedding(images,labels):
+
+def image_embedding(images,label,mode,filename):
   transform = get_transform()
-  background = []
-  lbls = []
-  for image,label in zip(images,labels):
-    lbl=[0,0]
-    bkg = np.zeros((480,640),np.uint8)
-    start = time.time()
-    points = person_detector(image)
-    #print('time ',time.time() - start)
-    for p in range(len(points)-1):
-      bkg = cv2.line(bkg,points[p],points[p+1],(255,0,0),5)
-    bkg = transform(Image.fromarray(bkg))
-    lbl[1] = 1 if 1 in label else 0
-    lbl[0] = 1 if 1 not in label else 0
-    background.append(bkg)
-    lbls.append(lbl)
-  #print(lbls)
-  return torch.stack(background,dim=0), torch.FloatTensor(lbls)
-
+  info = ['adl', 'fall']
+  trajectory_path = os.path.join(os.getcwd(), 'trajectories', mode, info[label])
+  safedirs(trajectory_path)
+  logger_t.info(f'creating trajectory image at {trajectory_path}')
+  bkg = np.zeros((480,640),np.uint8)
+  t=t_set()
+  points = person_detector(images,label,mode,filename) 
+  logger.info(f'Elapsed time for Bounding box calculation is {t_diff(t)}')
+  for p in range(len(points)-1):
+    bkg = cv2.line(bkg,points[p],points[p+1],(255,0,0),5)
+  bkg = transform(tf.expand_dims(bkg,2))
+  path = os.path.join(trajectory_path, filename+'.jpg')
+  cv2.imwrite(path, bkg.numpy())
